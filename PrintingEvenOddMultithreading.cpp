b80cda3d436719d732dbfa59f91d1981073bae18
Tauqeer <tauqeer.akhtar@mercedes-benz.com>
2024-01-16 10:51:18 +0530

    commit 3

--- PrintingEvenOddMultithreading.cpp
+++ PrintingEvenOddMultithreading.cpp
@@ -21,9 +21,11 @@ int main() {
   for (int i = 0; i <= 5; ++i) {  // Defect: Accessing array out of bounds
         std::cout << array[i] << std::endl;
     }
-  // int arr[5] = {1, 2, 3, 4, 5};
-  // int *ptr = arr;
-  // ptr = ptr + 2;  // Violates MISRA C++ Rule 5-0-15
+  int arr[5] = {1, 2, 3, 4, 5};
+  int *ptr = arr;
+  ptr = ptr + 2;  // Violates MISRA C++ Rule 5-0-15
+    
+  int val = *ptr;
 
 
 
